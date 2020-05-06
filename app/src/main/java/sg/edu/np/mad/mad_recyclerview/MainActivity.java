f27e5dc24fe6d3c7e730ae85cdeb7f061190f61c
westwq <twq2@np.edu.sg>
2020-05-06 10:32:47 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/np/mad/mad_recyclerview/MainActivity.java
@@ -0,0 +1,35 @@
+package sg.edu.np.mad.mad_recyclerview;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.inputmethod.InputMethodManager;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    /**
+     * Upon calling this method, the keyboard will retract
+     * and the recyclerview will scroll to the last item
+     *
+     * @param rv RecyclerView for scrolling to
+     * @param data ArrayList that was passed into RecyclerView
+     */
+    private void showNewEntry(RecyclerView rv, ArrayList data){
+        //scroll to the last item of the recyclerview
+        rv.scrollToPosition(data.size() - 1);
+
+        //auto hide keyboard after entry
+        InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+        imm.hideSoftInputFromWindow(rv.getWindowToken(), 0);
+    }
+}
